30f15a185ad7731c81cc4db955745c9942c3609a
OrtxzJxrdiel <elkin.ruiz1609@estu.unan.edu.ni>
2025-08-27 12:13:40 -0600

    Update actualizar.sh

--- xray-tunnel/modulos/acciones/actualizar.sh
+++ xray-tunnel/modulos/acciones/actualizar.sh
@@ -1,1 +1,7 @@
-
+actualizar_script() {
+  echo -e "${YELLOW}🔄 Actualizando menú desde GitHub...${NC}"
+  curl -s -o ~/xray-tunnel/xray-tunnel.sh \
+    https://raw.githubusercontent.com/OrtxzJxrdiel/config_termius/refs/heads/main/xray-tunnel/xray-tunnel.sh
+  chmod +x ~/xray-tunnel/xray-tunnel.sh
+  echo -e "${GREEN}✅ Menú actualizado correctamente.${NC}"
+}
